a57f83e2623055da6dedc9ae75619160b3935c87
jdeinnocenti <jdeinnocenti@gmail.com>
2013-06-02 13:34:48 -0300

    Proyecto con test de operaciones con complejos

---
+++ Wesselsoft_tests/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/javier/git/MatSup/WesselSoft/Debug/src/funciones_complejos.o /home/javier/git/MatSup/WesselSoft/Debug/src/list.o
+
+LIBS := -lm
+
