36f58e31b3a6aabfe5f61ff7141605678d231ea2
jdeinnocenti <jdeinnocenti@gmail.com>
2013-06-08 15:40:11 -0300

    Funcion transferencia

---
+++ Includes/transferencia.h
@@ -0,0 +1,18 @@
+/*
+ * transferencia.h
+ *
+ *  Created on: 07/06/2013
+ *      Author: javier
+ */
+
+#ifndef TRANSFERENCIA_H_
+#define TRANSFERENCIA_H_
+
+#include "complejo.h"
+#include "list.h"
+
+tComplejo transferencia(t_list *ceros, t_list *polos, double k, tComplejo punto);
+
+
+
+#endif /* TRANSFERENCIA_H_ */
---
+++ src/transferencia.c
@@ -0,0 +1,36 @@
+/*
+ * transferencia.c
+ *
+ *  Created on: 07/06/2013
+ *      Author: javier
+ */
+
+#include "transferencia.h"
+
+tComplejo transferencia(t_list *ceros, t_list *polos, double k, tComplejo punto)
+{
+	tComplejo dividendo, divisor, resultado;
+
+	//Obtenemos el dividendo de la función
+	if (list_is_empty(ceros)){
+		dividendo.stBinomica.dReal = k;
+		dividendo.stBinomica.dImaginaria = 0;
+		}
+		else {
+			dividendo = *restar(&punto, (tComplejo *) list_remove(ceros, 0));
+			while (list_is_empty(ceros) == 0 ){                    //mientras que la lista no este vacía
+				dividendo = *multiplicar(restar(&punto,(tComplejo *) list_remove(ceros, 0)), &dividendo);
+				};
+			dividendo.stBinomica.dReal = dividendo.stBinomica.dReal * k;
+			dividendo.stBinomica.dImaginaria = dividendo.stBinomica.dImaginaria * k;
+			};
+	//Obtenemos el divisor de la función
+	divisor = *restar(&punto, (tComplejo *) list_remove(polos, 0));
+	while (list_is_empty(polos) == 0 ){                    //mientras que la lista no este vacía
+		divisor = *multiplicar(restar(&punto, (tComplejo *) list_remove(polos, 0)), &divisor);
+		};
+	//Obtenemos el resultado
+	resultado = *dividir(&dividendo, &divisor);
+	return resultado;
+}
+
